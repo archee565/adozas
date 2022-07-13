3e5205664ee49901ca300fa6d2aabb4e4750ae1c
archee565 <75748397+archee565@users.noreply.github.com>
2022-07-13 02:56:42 +0200

    javitva

--- adozo.py
+++ adozo.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
 
+# forrasok:
+# https://www.hypercortex.hu/atalanyadozas-ujratoltve-2022/
+
+
 def szjaado(bevetel):
     egyebkoltseg = 0
     ugyvezeto_brutto = 12*200000
@@ -19,11 +23,13 @@ def szjaado(bevetel):
 
 
 def atalany(bevetel):
+    minimalber_brutto = 200000
     koltseghanyad = 0.4
+    mentesitett = minimalber_brutto*12/2
     adoalap = (1-koltseghanyad)*bevetel
-    szja = adoalap*0.15
-    tbj = adoalap*0.185
-    szocho = min(adoalap*0.13,628000)
+    szja = max( (adoalap-mentesitett)*0.15,0)
+    tbj = max(adoalap*0.185,minimalber_brutto*0.185)
+    szocho = max(min(adoalap*0.13,628000),minimalber_brutto*1.125*0.13)
     ipa = adoalap*1.2*0.02
     if bevetel>12*10*200000:
         return bevetel
@@ -37,7 +43,7 @@ cy_atalany = []
 
 for x in range(1,1000):
     bevetel = 1 + 26*1e6*(x/1000)
-    cx.append(bevetel)
+    cx.append(bevetel/1e6)
     cy_szja.append(szjaado(bevetel)/bevetel*100)
     cy_atalany.append(  atalany(bevetel)/bevetel * 100 )
 
@@ -45,7 +51,7 @@ for x in range(1,1000):
 plt.plot(cx, cy_szja)
 plt.plot(cx, cy_atalany)
 plt.ylim([0, 100])
-plt.xlabel('bevetel')
+plt.xlabel('éves bevetel (millio HUF)')
 plt.ylabel('ado%')
 plt.title('TAO+SZJA vs átalány')
 plt.show()
